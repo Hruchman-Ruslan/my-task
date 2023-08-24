cac061fa145d9277eb4785c303264db5a0b2e6b5
Ruslan <Giovingo92@gmail.com>
2023-08-24 18:16:11 +0300

    basic settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { Global } from '@emotion/react';
+
+import 'modern-normalize';
+import { GlobalStyles } from './styles/GlobalStyles.styled';
+import { App } from 'components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Global styles={GlobalStyles} />
     <App />
   </React.StrictMode>
 );
